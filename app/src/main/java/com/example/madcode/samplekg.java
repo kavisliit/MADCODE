7a94cb7738f9c665c5885e82adfd958b814b47cd
Kaveen Ranaweera <Kaveen Ranaweera@LAPTOP-V3J2ASRQ>
2021-09-14 00:23:51 +0530

    kaveen sample push2

---
+++ app/src/main/java/com/example/madcode/samplekg.java
@@ -0,0 +1,14 @@
+package com.example.madcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class samplekg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samplekg);
+    }
+}
