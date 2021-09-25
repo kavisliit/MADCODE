c3261b67221d3116124f98eae7b67a0020c44bbf
SudewaJay <it20062774@my.sliit.lk>
2021-09-26 01:36:33 +0530

    Sudewaj

--- app/src/main/java/com/example/madcode/MainActivity.java
+++ app/src/main/java/com/example/madcode/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.madcode;
 
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.madcode.Article.ArticleMainPageCustomer;
+import com.example.madcode.Article.My_Article;
+import com.example.madcode.User.user_profile;
+
+
+
 
 public class MainActivity extends nav_activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+
 
     }
+
+
+
+
+    public void MainToMyCreate(View view){
+        Intent intent = new Intent(this, My_Article.class);
+        startActivity(intent);
+    }
+    public void MainToArticles(View view){
+        Intent intent = new Intent(this, ArticleMainPageCustomer.class);
+        startActivity(intent);
+    }
+    public void MainToEventMain(View view){
+        Intent intent = new Intent(this, user_profile.class);
+        startActivity(intent);
+    }
+
+
 
 }
